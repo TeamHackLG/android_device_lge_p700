82c4c071af04475eee91f26e9586c43a9ff60d4f
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-13 22:49:36 -0300

    copyright: Add it for all files
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,17 +1,16 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Boot animation
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -19,6 +18,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/p700/full_p700.mk)
 
+# Bootanimation
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
--- device.mk
+++ device.mk
@@ -1,24 +1,19 @@
-# Copyright (C) 2013 The Android Open-Source Project
-# Copyright (C) 2016 The TeamVee-u0 Project
+# Copyright 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# This file includes all definitions that apply to ALL p700 devices, and
-# are also specific to p700 devices
-#
+# This file includes all definitions that apply to ALL L7 devices
 # Everything in this directory will become public
-
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/p700/overlay
 
