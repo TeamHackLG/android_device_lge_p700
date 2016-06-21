f28e2fc35a88a228b44723f43ac07632102062f8
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-20 21:14:13 -0300

    init_u0: Fix setting under bugged LolliPop
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- init/init_u0.c
+++ init/init_u0.c
@@ -36,28 +36,29 @@
 #include "util.h"
 
 void vendor_load_properties() {
-    char gversionbb[92];
-    char dversionbb[92];
+    //char gversionbb[92];
+    //char dversionbb[92];
     char o_gversionbb[92];
     char o_dversionbb[92];
-    FILE *fp;
+    //FILE *fp;
 
-    fp = popen("/system/xbin/printf $(/system/xbin/strings /dev/block/mmcblk0p12 | /system/bin/grep -e '-V10' -e '-V20')", "r");
-    fgets(gversionbb, sizeof(gversionbb), fp);
-    pclose(fp);
-    property_set("gsm.version.baseband", gversionbb);
+    //fp = popen("/system/xbin/printf $(/system/xbin/strings /dev/block/mmcblk0p12 | /system/bin/grep -e '-V10' -e '-V20')", "r");
+    //fgets(gversionbb, sizeof(gversionbb), fp);
+    //pclose(fp);
+    //property_set("gsm.version.baseband", gversionbb);
 
-    fp = popen("/system/xbin/printf $(/system/bin/getprop gsm.version.baseband | /system/bin/grep -o -e 'P700' -e 'P705')", "r");
-    fgets(dversionbb, sizeof(dversionbb), fp);
-    pclose(fp);
-    property_set("ro.product.device", dversionbb);
-    property_set("ro.product.model", dversionbb);
+    //fp = popen("/system/xbin/printf $(/system/bin/getprop gsm.version.baseband | /system/bin/grep -o -e 'P700' -e 'P705')", "r");
+    //fgets(dversionbb, sizeof(dversionbb), fp);
+    //pclose(fp);
+    //property_set("ro.product.device", dversionbb);
+    //property_set("ro.product.model", dversionbb);
 
-    if (strlen(gversionbb) == 0) {
-        property_set("gsm.version.baseband", "V20");
+    //if (strlen(gversionbb) == 0) {
+        //property_set("gsm.version.baseband", "V20");
+        property_set("gsm.version.baseband", "No way to get - Unknown");
         property_set("ro.product.device", "u0");
         property_set("ro.product.model", "u0");
-    };
+    //};
 
     property_get("gsm.version.baseband", o_gversionbb);
     property_get("ro.product.device", o_dversionbb);
